17d8e49cd99a95b8d022fbdca3c53a138aea76e4
Tomoyuki Sakurai <y@trombik.org>
2018-04-06 22:17:10 +0900

    implement dhcpd and unbound

---
+++ spec/serverspec/ns/000_default_spec.rb
@@ -0,0 +1,54 @@
+require_relative "../spec_helper"
+
+domain_name = case test_environment
+              when "virtualbox"
+                "i.trombik.org"
+              end
+domain_name_servers = case test_environment
+                      when "virtualbox"
+                        "172.16.100.254"
+                      end
+dhcp_flags = case test_environment
+             when "virtualbox"
+               "em1"
+             end
+
+subnets = case test_environment
+          when "virtualbox"
+            [
+              {
+                network: "172.16.100.0", netmask: "255.255.255.0", from: "172.16.100.100", to: "172.16.100.200",
+                option: [
+                  "broadcast-address 172.16.100.255",
+                  "routers 172.16.100.254"
+                ]
+              }
+            ]
+          end
+dig_command = case os[:family]
+              when "openbsd"
+                "dig"
+              when "freebsd"
+                "drill"
+              end
+
+describe file "/etc/rc.conf.local" do
+  it { should be_file }
+  its(:content) { should match(/^dhcpd_flags=#{dhcp_flags}$/) }
+end
+
+describe file "/etc/dhcpd.conf" do
+  it { should be_file }
+  its(:content) { should match(/^option domain-name "#{domain_name}";$/) }
+  its(:content) { should match(/^option domain-name-servers #{domain_name_servers};$/) }
+  its(:content) { should match(/^default-lease-time 3600;$/) }
+  its(:content) { should match(/^max-lease-time 7200;$/) }
+  subnets.each do |subnet|
+    its(:content) { should match(/^subnet #{subnet[:network]} netmask #{subnet[:netmask]} {/) }
+  end
+end
+
+describe command "#{dig_command} example.org @10.0.2.15" do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match(/^;.*status: NOERROR/) }
+end
