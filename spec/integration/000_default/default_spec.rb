6e6db0d71988914207f45b603b0126b88ea66aa5
Tomoyuki Sakurai <y@trombik.org>
2018-10-19 11:46:05 +0900

    test internal DNS on ns as well

--- spec/integration/000_default/default_spec.rb
+++ spec/integration/000_default/default_spec.rb
@@ -11,8 +11,19 @@ when "virtualbox"
         end
       end
     end
-    all_hosts_in("client").each do |s|
+    (all_hosts_in("ns") + all_hosts_in("client")).each do |s|
       describe s do
+        let(:dig_command) do
+          case current_server.ssh_exec "uname -s".chomp
+          when /FreeBSD/
+            "drill -t"
+          when /OpenBSD/
+            "dig"
+          else
+            raise "Unknown OS"
+          end
+        end
+
         it "has a list of internal DNS server in /etc/resolv.conf" do
           r = current_server.ssh_exec "cat /etc/resolv.conf"
           all_hosts_in("ns").each do |ns_server|
@@ -24,14 +35,14 @@ when "virtualbox"
           yaml = YAML.load_file(playbooks_path + "group_vars" + "#{test_environment}.yml")
           expect(yaml["project_hosts"].length).not_to eq 0
           yaml["project_hosts"].each do |host|
-            r = current_server.ssh_exec "drill a #{host['name']}.i.trombik.org"
+            r = current_server.ssh_exec "#{dig_command} #{host['name']}.i.trombik.org"
             expect(r).to match(/^;; ANSWER SECTION:\n#{host['name']}\.i\.trombik\.org\.\s+86400\s+IN\s+A\s+#{host['ipv4']}$/)
           end
         end
 
         it "resolves external RR over TCP" do
-          r = current_server.ssh_exec "drill -t a example.org"
-          expect(r).to match(/rcode: NOERROR/)
+          r = current_server.ssh_exec "#{dig_command} a example.org"
+          expect(r).to match(/(rcode|status): NOERROR/)
         end
       end
     end
